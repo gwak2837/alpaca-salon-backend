dc1bc4a3c52f88deae2f8dc4c802119ef69044c2
Taeuk Gwak <gwak2837@kakao.com>
2021-11-14 16:44:20 +0900

    Fixing kakao token validation

--- src/express/oauth.ts
+++ src/express/oauth.ts
@@ -1,15 +1,10 @@
-import querystring from 'querystring'
-
 import { Express } from 'express'
 import fetch from 'node-fetch'
-import passport from 'passport'
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
 
 import { poolQuery } from '../database/postgres'
 import { generateJWT } from '../utils/jwt'
 import findKakaoUser from './sql/findKakaoUser.sql'
 import registerKakaoUser from './sql/registerKakaoUser.sql'
-import socialLogin from './sql/socialLogin.sql'
 
 function encodeGender(gender: string) {
   switch (gender) {
@@ -24,6 +19,10 @@ function encodeGender(gender: string) {
 
 function verifyTargetCustomer(user: any) {
   return true
+}
+
+function hasRequiredInfo(user: any) {
+  return user.nickname && user.phone
 }
 
 async function fetchKakaoUserToken(code: string) {
@@ -57,6 +56,7 @@ export function setOAuthStrategies(app: Express) {
     }
 
     const kakaoUserToken = await fetchKakaoUserToken(req.query.code as string)
+    console.log('👀 - kakaoUserToken', kakaoUserToken)
     const kakaoUserInfo = await fetchKakaoUserInfo(kakaoUserToken.access_token as string)
     console.log('👀 - kakaoUserInfo', kakaoUserInfo)
 
@@ -73,7 +73,7 @@ export function setOAuthStrategies(app: Express) {
       const jwt = await generateJWT({ userId: kakaoUser.id })
 
       // nickname 또는 phone 정보가 없는 경우
-      if (!kakaoUser.nickname || !kakaoUser.phone) {
+      if (!hasRequiredInfo(kakaoUser)) {
         return res.redirect(
           `${frontendUrl}/oauth/register?${encodeURIComponent(
             JSON.stringify({ jwt, user: kakaoUser }, null, 2)
@@ -108,7 +108,7 @@ export function setOAuthStrategies(app: Express) {
     const jwt = await generateJWT({ userId: newKakaoUser.id })
 
     // nickname 또는 phone 정보가 없는 경우
-    if (!newKakaoUser.nickname || !newKakaoUser.phone) {
+    if (!hasRequiredInfo(newKakaoUser)) {
       return res.redirect(
         `${frontendUrl}/oauth/register?${encodeURIComponent(
           JSON.stringify({ jwt, user: newKakaoUser }, null, 2)
@@ -118,73 +118,4 @@ export function setOAuthStrategies(app: Express) {
 
     return res.redirect(`${frontendUrl}/oauth?${encodeURIComponent(jwt)}`)
   })
-
-  app.use(passport.initialize())
-
-  passport.use(
-    new GoogleStrategy(
-      {
-        clientID: process.env.GOOGLE_CLIENT_ID ?? '',
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-        callbackURL: `${process.env.BACKEND_URL}/oauth/google/callback`,
-      },
-      async (accessToken, refreshToken, profile, done) => {
-        if (profile.emails && (profile.emails[0] as any).verified) {
-          const { rows } = await poolQuery(socialLogin, [
-            profile.emails[0].value,
-            profile.displayName,
-            null,
-            null,
-            null,
-            profile.photos?.map((photo) => photo.value),
-            null,
-            null,
-            profile.emails[0].value,
-            null,
-            null,
-          ])
-
-          done(null, { id: rows[0].user_id })
-        } else {
-          // email not verified or no email
-
-          done(null)
-        }
-      }
-    )
-  )
-
-  passport.serializeUser((user, done) => {
-    console.log('user', user)
-    done(null, user)
-  })
-
-  passport.deserializeUser((obj, done) => {
-    console.log('obj', obj)
-    done(null, obj as any)
-  })
-
-  app.get(
-    '/oauth/google',
-    passport.authenticate('google', { scope: ['profile', 'email', 'openid'] })
-  )
-
-  app.get(
-    '/oauth/google/callback',
-    passport.authenticate('google', {
-      failureRedirect: '/aa',
-    }),
-    async (req, res) => {
-      const frontendUrl = req.headers.referer ?? ''
-
-      if (req.user) {
-        const query = querystring.stringify({
-          token: await generateJWT({ userId: (req.user as any).id }),
-        })
-        res.redirect(`${frontendUrl}/auth?${query}`)
-      } else {
-        res.redirect(`${frontendUrl}/signin`)
-      }
-    }
-  )
 }
