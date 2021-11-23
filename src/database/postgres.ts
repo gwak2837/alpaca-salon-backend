8d15df6a794d6fbf85aa940f81242f33ae9496cb
Taeuk Gwak <gwak2837@kakao.com>
2021-11-23 15:34:20 +0900

    Update postgres error logging

--- src/database/postgres.ts
+++ src/database/postgres.ts
@@ -7,15 +7,16 @@ export const pool = new Pool({
   connectionString: process.env.CONNECTION_STRING,
 })
 
-export async function poolQuery(query: string, values?: unknown[]) {
+export async function poolQuery(sql: string, values?: unknown[]) {
   if (process.env.NODE_ENV !== 'production') {
     // eslint-disable-next-line no-console
-    console.log(formatDate(new Date()), '-', query, values)
+    console.log(formatDate(new Date()), '-', sql, values)
   }
 
-  return pool.query(query, values).catch((error) => {
+  return pool.query(sql, values).catch((error) => {
     if (process.env.NODE_ENV === 'production') {
-      console.error(error)
+      console.error(error.message)
+      console.error(sql, values)
       throw new DatabaseQueryError('500 Database query error')
     } else {
       throw new DatabaseQueryError(error)
