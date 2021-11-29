325a943e8599169e2e06146741bdd73cfec21abb
Taeuk Gwak <gwak2837@kakao.com>
2021-11-29 20:35:03 +0900

    Update Query.ts

--- src/graphql/comment/Query.ts
+++ src/graphql/comment/Query.ts
@@ -6,10 +6,7 @@ import commentsByPost from './sql/commentsByPost.sql'
 
 export const Query: QueryResolvers<ApolloContext> = {
   commentsByPost: async (_, { postId }, { userId }) => {
-    const { rows } = await poolQuery(commentsByPost, [
-      postId,
-      '5349fcd3-264b-4f26-9203-f80f6ec6a8b5',
-    ])
+    const { rows } = await poolQuery(commentsByPost, [postId, userId])
 
     return commentORM(rows)
   },
