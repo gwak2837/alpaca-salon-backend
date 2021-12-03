a392cd28313ab4fe4736066e6aef95b9d15c71ad
Taeuk Gwak <gwak2837@kakao.com>
2021-12-03 16:03:07 +0900

    Update data

--- src/express/upload.ts
+++ src/express/upload.ts
@@ -8,7 +8,7 @@ import { bucketName } from '../utils/constants'
 const multer = Multer({
   storage: Multer.memoryStorage(),
   limits: {
-    fileSize: 5 * 1024 * 1024, // no larger than 5mb, you can change as needed.
+    fileSize: 10_000_000, // no larger than 10MB, you can change as needed.
   },
 })
 
